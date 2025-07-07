531130c5170e540691b4f53a16ee7d57d83ecb1a
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-07 12:47:49 +0530

    Auth Layout Changes

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -4,8 +4,8 @@ import logo from "@/assets/logo.png";
 const AuthLayout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="flex items-center justify-center w-full md:w-1/2 h-32 md:h-auto shadow-2xl bg-gradient-to-br from-white via-accent/30 to-white">
-        <img src={logo} alt="Logo" className="h-16 md:h-24 object-contain" />
+      <div className="flex items-center justify-center w-full md:w-1/3 py-6 shadow-2xl bg-gradient-to-br from-white via-accent/30 to-white">
+        <img src={logo} alt="Logo" className="h-12 md:h-16 object-contain" />
       </div>
       <div className="flex-1 flex items-center justify-center p-6">
         <div className="w-full max-w-md">{children}</div>
