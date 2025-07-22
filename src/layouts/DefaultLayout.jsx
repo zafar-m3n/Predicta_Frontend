5e6dae755c2f98c3076d06c912085e315a6c0e5b
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-22 19:46:27 +0530

    Refactoring

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -10,22 +10,17 @@ const DefaultLayout = ({ children }) => {
 
   return (
     <div className="flex h-screen bg-gray-50 font-dm-sans overflow-hidden relative dark:bg-gray-900">
-      {/* Sidebar */}
       <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-
-      {/* Mobile Overlay */}
       <div
         onClick={() => setMenuOpen(false)}
         className={`fixed inset-0 bg-black/50 z-40 md:hidden transition-opacity duration-300 ${
           menuOpen ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       />
-
-      {/* Main content */}
       <div className="flex-1 flex flex-col h-full">
         <TopbarDesktop />
         <TopbarMobile setMenuOpen={setMenuOpen} />
-        <main className="mt-10 p-4 md:p-6 overflow-y-auto flex-1">{children}</main>
+        <main className="mt-10 px-4 md:px-6 overflow-y-auto flex-1 py-5">{children}</main>
       </div>
     </div>
   );
